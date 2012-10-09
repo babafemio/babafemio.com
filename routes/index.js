b72ad35eb3f71d14ca8302c2d9ecf298a21cb788
Babafemi Ogungbamila <babafemi@ieee.org>
2012-10-09 12:33:05 +0100

    about to cleanup carousel

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,15 @@
  * GET home page.
  */
 
+var gallery = {[
+					url: '/img/capoeira-ipanema-brazil_58696_990x742.jpg',
+					caption: 'Ipanema, Brazil',
+					description: 'Brazilian capoeira is a traditional dance with amazing fight steps originating from slavery',
+					source: 'National Geographic',
+					source_url: 'www.nationalgeographic.com'
+
+				]};
+
 exports.index = function(req, res){
-  res.render('index', { title: 'babafemio.com' });
+  res.render('index', { title: 'babafemio.com' }, gallery);
 };
